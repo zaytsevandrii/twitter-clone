e3efe328649e5812ea2499d0e601de02fc34142a
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-03-24 19:59:27 +0000

    add registration form and zustand

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,20 +1,53 @@
 import React, { useCallback, useState } from "react"
+import Input from "../Input"
+import Modal from "../Modal"
+import useLoginModal from '../../hooks/useLoginModal'
 
 const LoginModal = () => {
+    const loginModal = useLoginModal();
+
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [isLoading, setIsLoading] = useState(false)
 
-    const onSubmit = useCallback(async() => {
+    const onSubmit = useCallback(async () => {
         try {
             setIsLoading(true)
         } catch (error) {
             console.log(error)
-        }finally{
+        } finally {
             setIsLoading(false)
         }
-    },[])
-    return <div>LoginModal</div>
+    }, [loginModal])
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+          <Input 
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            disabled={isLoading}  
+          />
+          <Input 
+            placeholder="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            disabled={isLoading} 
+          />
+        </div>
+      )
+    return (
+        <Modal
+        disabled={isLoading}
+        isOpen={loginModal.isOpen}
+        title="Login"
+        actionLabel="Sign in"
+        onClose={loginModal.onClose}
+        onSubmit={onSubmit}
+        body={bodyContent}
+      />
+    )
 }
 
 export default LoginModal
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app"
 import Layout from "../components/Layout "
 import Modal from "../components/Modal"
 import LoginModal from "../components/modals/LoginModal"
+import RegisterModal from "../components/modals/RegisterModal"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <><LoginModal/>
+        <RegisterModal/>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
